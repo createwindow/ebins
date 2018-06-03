dba1fa15704c6e915573c1c67651cd8aabd59a83
yi.guo <yi.guo@ushow.media>
2018-06-04 00:09:55 +0800

    update bash profile for mac
    Signed-off-by: yi.guo <yi.guo@ushow.media>

--- ebackup.sh
+++ ebackup.sh
@@ -6,7 +6,7 @@ if [ "$1" = "-mac" ]; then
     if [ ! -d "$DST_DIR/macOS" ]; then 
         mkdir $DST_DIR/macOS
     fi 
-    cp $HOME/.bash_profile $DST_DIR/macos/bash_profile
+    cp $HOME/.bash_profile $DST_DIR/macOS/bash_profile
 else
     cp /etc/profile $DST_DIR/profile
     cp /etc/network/interfaces $DST_DIR/interfaces
