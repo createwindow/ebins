4609caa57d1366fd9beb74d247ed1dda5a987c73
yi.guo <yi.guo@ushow.media>
2020-03-28 19:29:09 +0800

    daily update

--- e720p.sh
+++ e720p.sh
@@ -6,9 +6,9 @@ dir=`dirname "$1"`
 # echo "$file_name $dir"
 
 start_time=`date +%s`
-ffmpeg -i "$1" -vf scale=1280x720 "$dir"/"$file_name"1.mp4
+ffmpeg -i "$1" -vf scale=1280x720 "$dir"/"$file_name"6666.mp4
 end_time=`date +%s`
 
 used_time=$(($end_time - $start_time))
-printf -- '\033[32m\nConerting in $dir, used \033[0m\n'  $used_time;
+printf -- '\033[32m\nConerting for %s %s, used %s seconds.\033[0m\n' "$dir" "$file_name" "$used_time"
 
