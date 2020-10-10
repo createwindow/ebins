a47c3504133944f329980f818f86ed4d58d4f9d8
yi.guo <yi.guo@ushow.media>
2020-10-10 11:00:53 +0800

    daily update

--- zsmbuild.sh
+++ zsmbuild.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 dbg="yes"
-prd="starmaker"
+prd=""
 arg=""
 starmaker_dir="$HOME/workspace/code/ushow/starmaker-android-client"
 # starmaker_dir="/Volumes/Samsung_T5/starmaker-android-client"
+imissyo_dir="$HOME/workspace/code/ushow/voicex-android-client"
 
 usage()
 {
@@ -15,10 +16,11 @@ usage()
   echo "        -s, StarMaker."
   echo "        -t, StarMakerLite."
   echo "        -g, Sargam."
+  echo "        -i, iMissYo."
   echo "        -h, for help."
 }
 
-while getopts "drstgh" option
+while getopts "drstgih" option
 do
   case $option in
     h) usage
@@ -37,18 +39,29 @@ do
         exit 1
       fi
       prd="starmaker"
+      dir="$starmaker_dir"
     ;;
     t) if [ "$prd" = "sargam" ]; then
         usage
         exit 1
       fi
       prd="starmakerlite"
+      dir="$starmaker_dir"
     ;;
     g) if [ "$prd" = "starmakerlite" ]; then
         usage
         exit 1
       fi
       prd="sargam"
+      dir="$starmaker_dir"
+    ;;
+    i) if [ "$prd" = "starmakerlite" -o  "$prd" = "sargam" -o "$prd" = "starmaker" ]; then
+        echo "$prd"
+        usage
+        exit 1
+      fi
+      prd="imissyo"
+      dir="$imissyo_dir"
     ;;
     ?) usage
        exit 1
@@ -60,38 +73,48 @@ if [ "$prd" = "starmaker" -a "$dbg" = "yes" ]; then
   echo "StarMaker     ===> yes"
   echo "Debug         ===> yes"
   arg="aPD"
-  rm -f "$starmaker_dir/app/build/outputs/apk/product/debug/*.apk"
+  rm -f "$dir/app/build/outputs/apk/product/debug/*.apk"
 elif [ "$prd" = "starmaker" -a "$dbg" = "no" ]; then
   echo "StarMaker     ===> yes"
   echo "Release       ===> yes"
   arg="aPR"
-  rm  "$starmaker_dir/app/build/outputs/apk/product/release/*.apk"
+  rm  "$dir/app/build/outputs/apk/product/release/*.apk"
 elif [ "$prd" = "starmakerlite" -a "$dbg" = "yes" ]; then
   echo "StarMakerLite ===> yes"
   echo "Debug         ===> yes"
   arg="aTD"
-  rm  "$starmaker_dir/app/build/outputs/apk/thevoice/debug/*.apk"
+  rm  "$dir/app/build/outputs/apk/thevoice/debug/*.apk"
 elif [ "$prd" = "starmakerlite" -a "$dbg" = "no" ]; then
   echo "StarMakerLite ===> yes"
   echo "Release       ===> yes"
   arg="aTR"
-  rm  "$starmaker_dir/app/build/outputs/apk/thevoice/release/*.apk"
+  rm  "$dir/app/build/outputs/apk/thevoice/release/*.apk"
 elif [ "$prd" = "sargam" -a "$dbg" = "yes" ]; then
   echo "sargam        ===> yes"
   echo "Debug         ===> yes"
   arg="assembleSargamDebug"
-  rm  "$starmaker_dir/app/build/outputs/apk/sargam/debug/*.apk"
+  rm  "$dir/app/build/outputs/apk/sargam/debug/*.apk"
 elif [ "$prd" = "sargam" -a "$dbg" = "no" ]; then
   echo "sargam        ===> yes"
   echo "Release       ===> yes"
   arg="assembleSargamRelease"
-  rm  "$starmaker_dir/app/build/outputs/apk/sargam/release/*.apk"
+  rm  "$dir/app/build/outputs/apk/sargam/release/*.apk"
+elif [ "$prd" = "imissyo" -a "$dbg" = "yes" ]; then
+  echo "iMissYo       ===> yes"
+  echo "Debug         ===> yes"
+  arg="aPD"
+  rm  "$dir/app/build/outputs/apk/product/debug/*.apk"
+elif [ "$prd" = "imissyo" -a "$dbg" = "no" ]; then
+  echo "iMissYo       ===> yes"
+  echo "Release       ===> yes"
+  arg="aPR"
+  rm  "$dir/app/build/outputs/apk/product/release/*.apk"
 else
   usage
   exit
 fi
 
-cd $starmaker_dir
+cd $dir
 ./gradlew "$arg" -PfastBuild
 # ./gradlew "$arg" -PminApi17
 
--- zsminstall.sh
+++ zsminstall.sh
@@ -2,10 +2,11 @@
 
 # starmaker_dir="/Users/guoyi/mywork/ushow/starmaker-android-client"
 dbg="yes"
-prd="starmaker"
+prd=""
 arg=""
 # starmaker_dir="/Volumes/Samsung_T5/starmaker-android-client"
 starmaker_dir="$HOME/workspace/code/ushow/starmaker-android-client"
+imissyo_dir="$HOME/workspace/code/ushow/voicex-android-client"
 
 usage()
 {
@@ -16,12 +17,13 @@ usage()
   echo "        -s, StarMaker."
   echo "        -t, StarMakerLite."
   echo "        -g, Sargam."
+  echo "        -i, iMissYo."
   echo "        -m, arm64."
   echo "        -p, just show apk dir."
   echo "        -h, for help."
 }
 
-while getopts "drstgmph" option
+while getopts "drstgimph" option
 do
   case $option in
     h) usage
@@ -42,18 +44,28 @@ do
         exit 1
       fi
       prd="starmaker"
+      dir="$starmaker_dir"
     ;;
     t) if [ "$prd" = "sargam" ]; then
         usage
         exit 1
       fi
       prd="starmakerlite"
+      dir="$starmaker_dir"
     ;;
     g) if [ "$prd" = "starmakerlite" ]; then
         usage
         exit 1
       fi
       prd="sargam"
+      dir="$starmaker_dir"
+    ;;
+    i) if [ "$prd" = "starmakerlite" -o  "$prd" = "sargam" -o "$prd" = "starmaker" ]; then
+        usage
+        exit 1
+      fi
+      prd="imissyo"
+      dir="$imissyo_dir"
     ;;
     p) only_position="yes"
     ;;
@@ -66,44 +78,58 @@ done
 if [ "$prd" = "starmaker" -a "$dbg" = "yes" ]; then
   echo "StarMaker     ===> yes"
   echo "Debug         ===> yes"
-  app="$starmaker_dir/app/build/outputs/apk/product/debug/productDebug-minApi21-armeabi-v7a.apk"
+  app="$dir/app/build/outputs/apk/product/debug/productDebug-minApi21-armeabi-v7a.apk"
   if [ "$arm64" = "yes" ]; then
-    app="$starmaker_dir/app/build/outputs/apk/product/debug/productDebug-minApi21-arm64-v8a.apk"
+    app="$dir/app/build/outputs/apk/product/debug/productDebug-minApi21-arm64-v8a.apk"
   fi
 elif [ "$prd" = "starmaker" -a "$dbg" = "no" ]; then
   echo "StarMaker     ===> yes"
   echo "Release       ===> yes"
-  app="$starmaker_dir/app/build/outputs/apk/product/release/productRelease-minApi21-armeabi-v7a.apk"
+  app="$dir/app/build/outputs/apk/product/release/productRelease-minApi21-armeabi-v7a.apk"
   if [ "$arm64" = "yes" ]; then
-    app="$starmaker_dir/app/build/outputs/apk/product/release/productRelease-minApi21-arm64-v8a.apk"
+    app="$dir/app/build/outputs/apk/product/release/productRelease-minApi21-arm64-v8a.apk"
   fi
 elif [ "$prd" = "starmakerlite" -a "$dbg" = "yes" ]; then
   echo "StarMakerLite ===> yes"
   echo "Debug         ===> yes"
-  app="$starmaker_dir/app/build/outputs/apk/thevoice/debug/thevoiceDebug-minApi21-armeabi-v7a.apk"
+  app="$dir/app/build/outputs/apk/thevoice/debug/thevoiceDebug-minApi21-armeabi-v7a.apk"
   if [ "$arm64" = "yes" ]; then
-    app="$starmaker_dir/app/build/outputs/apk/thevoice/debug/thevoiceDebug-minApi21-arm64-v8a.apk"
+    app="$dir/app/build/outputs/apk/thevoice/debug/thevoiceDebug-minApi21-arm64-v8a.apk"
   fi
 elif [ "$prd" = "starmakerlite" -a "$dbg" = "no" ]; then
   echo "StarMakerLite ===> yes"
   echo "Release       ===> yes"
-  app="$starmaker_dir/app/build/outputs/apk/thevoice/release/thevoiceRelease-minApi21-armeabi-v7a.apk"
+  app="$dir/app/build/outputs/apk/thevoice/release/thevoiceRelease-minApi21-armeabi-v7a.apk"
   if [ "$arm64" = "yes" ]; then
-    app="$starmaker_dir/app/build/outputs/apk/thevoice/release/thevoiceRelease-minApi21-arm64-v8a.apk"
+    app="$dir/app/build/outputs/apk/thevoice/release/thevoiceRelease-minApi21-arm64-v8a.apk"
   fi
 elif [ "$prd" = "sargam" -a "$dbg" = "yes" ]; then
   echo "Sargam        ===> yes"
   echo "Debug         ===> yes"
-  app="$starmaker_dir/app/build/outputs/apk/sargam/debug/sargamDebug-minApi21-armeabi-v7a.apk"
+  app="$dir/app/build/outputs/apk/sargam/debug/sargamDebug-minApi21-armeabi-v7a.apk"
   if [ "$arm64" = "yes" ]; then
-    app="$starmaker_dir/app/build/outputs/apk/sargam/debug/sargamDebug-minApi21-arm64-v8a.apk"
+    app="$dir/app/build/outputs/apk/sargam/debug/sargamDebug-minApi21-arm64-v8a.apk"
   fi
 elif [ "$prd" = "sargam" -a "$dbg" = "no" ]; then
   echo "sargam        ===> yes"
   echo "Release       ===> yes"
-  app="$starmaker_dir/app/build/outputs/apk/sargam/release/sargamRelease-minApi21-armeabi-v7a.apk"
+  app="$dir/app/build/outputs/apk/sargam/release/sargamRelease-minApi21-armeabi-v7a.apk"
   if [ "$arm64" = "yes" ]; then
-    app="$starmaker_dir/app/build/outputs/apk/sargam/release/sargamRelease-minApi21-arm64-v8a.apk"
+    app="$dir/app/build/outputs/apk/sargam/release/sargamRelease-minApi21-arm64-v8a.apk"
+  fi
+elif [ "$prd" = "imissyo" -a "$dbg" = "yes" ]; then
+  echo "iMissYo       ===> yes"
+  echo "Debug         ===> yes"
+  app="$dir/app/build/outputs/apk/product/debug/imissyoDebug-minApi21-armeabi-v7a.apk"
+  if [ "$arm64" = "yes" ]; then
+    app="$dir/app/build/outputs/apk/sargam/debug/imissyoDebug-minApi21-arm64-v8a.apk"
+  fi
+elif [ "$prd" = "imissyo" -a "$dbg" = "no" ]; then
+  echo "iMissYo       ===> yes"
+  echo "Release       ===> yes"
+  app="$dir/app/build/outputs/apk/product/release/imissyoDebug-minApi21-armeabi-v7a.apk"
+  if [ "$arm64" = "yes" ]; then
+    app="$dir/app/build/outputs/apk/sargam/release/imissyoDebug-minApi21-arm64-v8a.apk"
   fi
 else
   usage
